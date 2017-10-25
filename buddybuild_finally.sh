2055f98a065de45aea8e9e601d87c7317b970248
Christopher Stott <christopherstott@gmail.com>
2017-10-25 06:55:12 -0400

    Echos

--- buddybuild_finally.sh
+++ buddybuild_finally.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+echo Echoing the contents of vendor/bundle
 ls vendor/bundle
+echo DONE echoing
 
+echo chruby!
 chruby 2.3.1
+
+echo bundle install
 bundle install --path vendor/bundle
+
+echo running danger
 bundle exec danger --fail-on-errors=true  
